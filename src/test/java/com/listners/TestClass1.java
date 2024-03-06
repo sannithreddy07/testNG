780c506589a37c1dd8926779396d9a0f87d76170
Sannith Reddy <156191001+sannithreddy07@users.noreply.github.com>
2024-03-06 17:04:35 +0530

    Update TestClass1.java

--- src/test/java/com/listners/TestClass1.java
+++ src/test/java/com/listners/TestClass1.java
@@ -3,8 +3,8 @@ package com.listners;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
-//@Listeners(ItestlistnerClass.class)
-
+//@Listeners(ItestlistnerClass.class) //we can use this annotation to execute the program without testng.xml
+					//if you don't want to use we can use ,listners> tags in testng.xml
 public class TestClass1 {
 	@Test 
 	public void test1() {
